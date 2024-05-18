59b3eb5e05b3994ff88c34803b9d130de41a2104
nickbelwin <belwalkarsarvesh@gmail.com>
2024-05-18 10:11:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express= require('express');
 const app = express();
 const cors = require('cors');
+const corsConfig={
+    origin:"*",
+    credential:true,
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
+}
 require('dotenv').config();
 const port= process.env.PORT ;
+
+
 app.use(cors());
 app.use(express.json());
 const dbConnect= require('./database');
